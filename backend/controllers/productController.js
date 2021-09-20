7b4fa49e63275b949064dc0f7eef63095ab53e61
Antonio Loza <antonioloza@Antonios-MacBook-Pro-2.local>
2021-09-20 12:40:37 -0700

    Ready to Deploy

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,9 +7,21 @@ import Product from '../models/productModel.js'
 //@access Public route
 
 const getProducts = asyncHandler(async(req, res) => {
-    const products = await Product.find({})
+  const pageSize = 4
+  const page = Number(req.query.pageNumber) || 1
+  
+  const keyword= req.query.keyword ? {
+    name: {
+      $regex: req.query.keyword,
+      $options: 'i'
+    }
+  } : {}
 
-    res.json(products)
+  const count = await Product.countDocuments({...keyword})
+  const products = await Product.find({...keyword}).limit(pageSize).skip(pageSize * (page -1 ))
+
+
+  res.json({products, page, pages: Math.ceil( count/ pageSize ) })
 })
 
 // @desc fetch all products 
@@ -101,4 +113,59 @@ const updateProduct = asyncHandler(async(req, res) => {
    
 })
 
-export {getProducts, getProductById, deleteProduct, createProduct, updateProduct}
+// @desc    Create new review
+// @route   POST /api/products/:id/reviews
+// @access  Private
+
+const createProductReview = asyncHandler(async (req, res) => {
+    const { rating, comment } = req.body
+  
+    const product = await Product.findById(req.params.id)
+  
+    if (product) {
+      const alreadyReviewed = product.reviews.find(
+        (r) => r.user.toString() === req.user._id.toString()
+      )
+      
+      const review = {
+        name: req.user.name,
+        rating: Number(rating),
+        comment,
+        user: req.user._id,
+      }
+  
+      if (alreadyReviewed) {
+        res.status(400)
+        throw new Error('Product already reviewed')
+      }
+  
+      
+  
+      product.reviews.push(review)
+  
+      product.numReviews = product.reviews.length
+  
+      product.rating =
+        product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+        product.reviews.length
+  
+      await product.save()
+      res.status(201).json({ message: 'Review added' })
+    } else {
+      res.status(404)
+      throw new Error('Product not found')
+    }
+  })
+
+// @desc    Get top rated product
+// @route   GET /api/products/top
+// @access  Public
+
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+
+  res.json(products)
+
+})
+
+export {getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview, getTopProducts}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,18 +31,21 @@ function App() {
         <Route path='/payment' component={PaymentScreen} />
         <Route path='/shipping' component={ShippingScreen} />
         <Route path='/placeorder' component={PlaceOrderScreen} />
-        <Route path='/' component={HomeScreen} exact/>
         <Route path='/product/:id' component={ProductScreen} />
         <Route path='/login' component={LoginScreen} />
         <Route path='/register' component={RegisterScreen} />
         <Route path='/profile' component={ProfileScreen} />
         <Route path='/cart/:id?' component={CartScreen} />
         <Route path='/admin/userlist' component={UserListScreen} />
-        <Route path='/admin/productlist' component={ProductListScreen} />
+        <Route path='/admin/productlist' component={ProductListScreen} exact/>
+        <Route path='/admin/productlist/:pageNumber' component={ProductListScreen} exact/>
         <Route path='/admin/orderlist' component={OrderListScreen} />
         <Route path='/admin/user/:id/edit' component={UserEditScreen} />
         <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
-
+        <Route path='/search/:keyword' component={HomeScreen} exact/>
+        <Route path='/page/:pageNumber' component={HomeScreen} exact/>
+        <Route path='/search/:keyword/page/:pageNumber' component={HomeScreen} exact/>
+        <Route path='/' component={HomeScreen} exact/>
       </Container>
     </main>
     <Footer />  
